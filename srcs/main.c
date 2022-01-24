feff5d977a7069de95b83915168c461e4f0bb288
Mateo Corso <mateocorso@gmail.com>
2022-01-24 18:04:07 +0100

    Check argc count

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,29 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 04:39:03 by mcorso            #+#    #+#             */
-/*   Updated: 2022/01/24 13:23:22 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/01/24 17:45:08 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	int		map;
 	t_vars	vars;
+	t_coord	*map;
 
+	if (argc != 2)
+		return (0);
+	map = open(*argv, O_RDONLY);
+	if (map < 0)
+	{
+		perror(NULL);
+		return (0);
+	}
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "FdF");
-	mlx_key_hook(vars.win, win_close, &vars);
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "FdF");
+	mlx_hook(vars.win, 2, 1L << 0, win_close, &vars);
+	mlx_hook(vars.win, 17, 0, win_destroy, &vars);
 	mlx_loop(vars.mlx);
 }
