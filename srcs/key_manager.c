50a90679ae06dbbd14645031fb29df84f1029089
Mateo Corso <mateocorso@gmail.com>
2022-01-24 13:31:40 +0100

    separating keyboard-related functions from main

--- srcs/key_manager.c
+++ srcs/key_manager.c
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   window_manager.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/22 04:39:03 by mcorso            #+#    #+#             */
-/*   Updated: 2022/01/24 13:23:22 by mcorso           ###   ########.fr       */
+/*   Created: 2022/01/24 12:18:44 by mcorso            #+#    #+#             */
+/*   Updated: 2022/01/24 13:24:25 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-int	main(void)
+int	win_close(int keycode, t_vars *vars)
 {
-	t_vars	vars;
-
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "FdF");
-	mlx_key_hook(vars.win, win_close, &vars);
-	mlx_loop(vars.mlx);
+	(void)vars;
+	if (keycode == 65307)
+		exit(0);
+	return (0);
 }
