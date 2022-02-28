30c1544ce67dc3401fc6517b16e4b4370d535840
Mateo Corso <mateocorso@gmail.com>
2022-02-28 21:09:00 +0100

    testing

--- srcs/chain.c
+++ srcs/chain.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 17:45:34 by mcorso            #+#    #+#             */
-/*   Updated: 2022/02/28 02:12:40 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/02/28 21:01:05 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	print_map(t_coord *first_node)
 	curr_node = first_node;
 	while (curr_node)
 	{
-		printf("%s", curr_node->line);
+		printf("%s\n", curr_node->line);
 		curr_node = curr_node->next;
 	}
 }
